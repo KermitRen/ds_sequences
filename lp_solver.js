d13bcff4ae65ec0e16502aa4bc5cb60b41ce099d
Frederik Baggesen <201804495@post.au.dk>
2023-02-13 12:27:14 +0100

    lp solve library

---
+++ lp_solver.js
@@ -0,0 +1,34 @@
+const highs_settings = {
+    locateFile: (file) => "https://lovasoa.github.io/highs-js/" + file
+  };
+
+const highs_promise = require("highs")(highs_settings)
+
+const PROBLEM = `Maximize
+obj:
+    x1 + 2 x2 + 4 x3 + x4
+Subject To
+c1: - x1 + x2 + x3 + 10 x4 <= 20
+c2: x1 - 4 x2 + x3 <= 30
+c3: x2 - 0.5 x4 = 0
+Bounds
+0 <= x1 <= 40
+2 <= x4 <= 3
+End`
+
+const PROBLEM2 = "Maximize \n obj: \n x1 \n Subject To \n c1: x1 <= 10 \n c2: x1 >= 11 \n End" 
+
+async function test() {
+    const highs = await highs_promise;
+    const sol = highs.solve(PROBLEM2);
+    console.log(sol)
+}
+
+async function solveLP(lp) {
+    const highs = await highs_promise;
+    const sol = highs.solve(lp)
+    console.log(sol)
+    return sol
+}
+
+module.exports = {test, solveLP}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const util = require("./util")
 const ds = require("./ds")
+const lp_solver = require("./lp_solver")
 
 
-ds.genDSseq(3,3)
-
+//ds.genDSseq(3,3)
+lp_solver.solveLP("Maximize \n obj: \n x1 \n Subject To \n c1: x1 >= 10 \n c2: x1 >= 11 \n End")
